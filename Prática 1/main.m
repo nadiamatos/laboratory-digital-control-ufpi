daaee29dd9a3d77a18383be2903def4e5fcd83ff
Nadia Matos <nadiamatos.18@gmail.com>
2019-04-14 09:42:29 -0300

    Add comments and plot graphs in main.m

--- Prática 1/main.m
+++ Prática 1/main.m
@@ -1,13 +1,27 @@
+% CODIGO DESENVOLVIDO: main.m
+% Este codigo e' o principal e chama todos os outros codigos disponiveis neste apendice.
+
 clear all; close all; clc;
 syms s
 
 Ts = 0.1;
-AMPLITUDE_OF_THE_STEP = 0.9; QUANTITY_SAMPLES = 300;
+AMPLITUDE_OF_THE_STEP = 90; QUANTITY_SAMPLES = 300;
 
+% fig coletado em laboratorio a partir de dados fornecidos pela planta
+fig = openfig('1_c_90.fig');
 
 % MALHA ABERTA
 k = 1; tau = 0.74225;
 Gs = tf(k, [tau 1]);
 Gz = transformStoZ(Gs, Ts);
 [output, time] = equationOfTheDifference(Gz, AMPLITUDE_OF_THE_STEP, QUANTITY_SAMPLES, Ts);
-plot(time, output, 'k', 'lineWidth', 2); grid on;
+
+% calculando o erro entre o que foi fornecido pela planta e o que foi estimado pelo algoritmo desenvolvido.
+erro = fig.Children.Children(2).YData - output;
+
+hold on;
+plot(time, output, 'k', 'lineWidth', 2);
+hold on;
+plot(time, erro, 'c', 'lineWidth', 2);
+grid on; title('Grafico da Velocidade');
+xlabel('Tempo (s)'); ylabel('Velocidade (RPS)');
